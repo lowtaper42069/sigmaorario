27565af01cb2e51dcd074f3a9fef8c2807bd88a9
5PAC3 <matteo.on.the.sk8@gmail.com>
2026-03-24 20:12:42 +0100

    primo test login

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://gjdoopyjwlnfzmjhwcux.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqZG9vcHlqd2xuZnptamh3Y3V4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNjQ1NTcsImV4cCI6MjA4OTk0MDU1N30.Gel_R0yn2dLjI1cubEATB0jaSQN8-pS8NlOrWj8T4FM';
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
